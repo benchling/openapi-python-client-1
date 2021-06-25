9b0c2cce7f94999e9da4dd0d494ec229d02daa82
George Murray <george@benchling.com>
2021-06-25 16:11:11 -0700

    Draft for required/unset fields override

--- openapi_python_client/templates/model.pyi
+++ openapi_python_client/templates/model.pyi
@@ -1,4 +1,5 @@
-from typing import Any, Dict, Type, TypeVar
+from ..extensions import NotPresentError
+from typing import Any, Dict, Optional, Type, TypeVar
 
 {% if model.additional_properties %}
 from typing import List
@@ -25,16 +26,16 @@ class {{ model.reference.class_name }}:
     """ {{ model.description }} """
     {% for property in model.required_properties + model.optional_properties %}
     {% if property.default is none and property.required %}
-    {{ property.to_string() }}
+    _{{ property.to_string() }}
     {% endif %}
     {% endfor %}
     {% for property in model.required_properties + model.optional_properties %}
     {% if property.default is not none or not property.required %}
-    {{ property.to_string() }}
+    _{{ property.to_string() }}
     {% endif %}
     {% endfor %}
     {% if model.additional_properties %}
-    additional_properties: Dict[str, {{ additional_property_type }}] = attr.ib(init=False, factory=dict)
+    _additional_properties: Dict[str, {{ additional_property_type }}] = attr.ib(init=False, factory=dict)
     {% endif %}
 
 
@@ -42,9 +43,9 @@ class {{ model.reference.class_name }}:
         {% for property in model.required_properties + model.optional_properties %}
         {% if property.template %}
         {% from "property_templates/" + property.template import transform %}
-        {{ transform(property, "self." + property.python_name, property.python_name) | indent(8) }}
+        {{ transform(property, "self._" + property.python_name, property.python_name) | indent(8) }}
         {% else %}
-        {{ property.python_name }} =  self.{{ property.python_name }}
+        {{ property.python_name }} =  self._{{ property.python_name }}
         {% endif %}
         {% endfor %}
 
@@ -52,10 +53,10 @@ class {{ model.reference.class_name }}:
         {% if model.additional_properties %}
         {% if model.additional_properties.template %}
         {% from "property_templates/" + model.additional_properties.template import transform %}
-        for prop_name, prop in self.additional_properties.items():
+        for prop_name, prop in self._additional_properties.items():
             {{ transform(model.additional_properties, "prop", "field_dict[prop_name]") | indent(12) }}
         {% else %}
-        field_dict.update(self.additional_properties)
+        field_dict.update(self._additional_properties)
         {% endif %}
         {% endif %}
         field_dict.update({
@@ -105,9 +106,9 @@ class {{ model.reference.class_name }}:
             {{ construct(model.additional_properties, "prop_dict") | indent(12) }}
             additional_properties[prop_name] = {{ model.additional_properties.python_name }}
 
-        {{model.reference.module_name}}.additional_properties = additional_properties
+        {{model.reference.module_name}}._additional_properties = additional_properties
     {% else %}
-        {{model.reference.module_name}}.additional_properties = d
+        {{model.reference.module_name}}._additional_properties = d
     {% endif %}
 {% endif %}
         return {{model.reference.module_name}}
@@ -115,17 +116,59 @@ class {{ model.reference.class_name }}:
     {% if model.additional_properties %}
     @property
     def additional_keys(self) -> List[str]:
-        return list(self.additional_properties.keys())
+        return list(self._additional_properties.keys())
 
     def __getitem__(self, key: str) -> {{ additional_property_type }}:
-        return self.additional_properties[key]
+        return self._additional_properties[key]
 
     def __setitem__(self, key: str, value: {{ additional_property_type }}) -> None:
-        self.additional_properties[key] = value
+        self._additional_properties[key] = value
 
     def __delitem__(self, key: str) -> None:
-        del self.additional_properties[key]
+        del self._additional_properties[key]
 
     def __contains__(self, key: str) -> bool:
-        return key in self.additional_properties
+        return key in self._additional_properties
     {% endif %}
+
+{% macro required_property_type(prop) %}
+{%- if prop.nullable -%}
+Optional[{{prop.get_base_type_string()}}]
+{%- else -%}
+{{prop.get_base_type_string()}}
+{%- endif -%}
+{% endmacro %}
+
+    {% if model.additional_properties %}
+    def get(self, key, default:Optional[{{ additional_property_type }}]=None) -> Optional[{{ additional_property_type }}]:
+        return self._additional_properties.get(key, default)
+    {% endif %}
+
+    {% for property in model.required_properties + model.optional_properties %}
+    {% if property.required %}
+    @property
+    def {{property.python_name}}(self) -> {{required_property_type(property)}}:
+        return self._{{property.python_name}}
+    {% else %}
+    @property
+    def {{property.python_name}}(self) -> {{required_property_type(property)}}:
+        if isinstance(self._{{property.python_name}}, Unset):
+            raise NotPresentError(self, "{{property.python_name}}")
+        return self._{{property.python_name}}
+    {% endif %}
+
+    {#
+        The ifs are split here because if you don't put your setter right next to your getter, mypy complains with a
+        completely unhelpful error message.
+    #}
+    @{{property.python_name}}.setter
+    def {{property.python_name}}(self, value:{{property.get_type_string()}}):
+        self._{{property.python_name}} = value
+
+    {% if not property.required %}
+    @property
+    def {{property.python_name}}_is_set(self) -> bool:
+        return not isinstance(self._{{property.python_name}}, Unset)
+    {% endif %}
+
+    {% endfor %}
